f87a5678581d737200c7bbefa1eec0abdb7a1b0f
Jeff Triplett <jeff.triplett@gmail.com>
2020-08-16 09:07:41 -0500

    :elephant: Migrates ForeignKey to OneToOneField

--- races/models.py
+++ races/models.py
@@ -215,8 +215,8 @@ class Racer(MachineTagMixin):
 
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
-    trailhawk = models.ForeignKey('members.Member', unique=True, null=True, blank=True,
-                                  help_text='If racer is a trailhawk select profile.')
+    trailhawk = models.OneToOneField('members.Member', null=True, blank=True,
+                                     help_text='If racer is a trailhawk select profile.')
     email = models.CharField(max_length=50, blank=True, null=True)
     phone = models.CharField(max_length=13, blank=True, null=True)
     gender = models.IntegerField(choices=GENDER_CHOICES)
