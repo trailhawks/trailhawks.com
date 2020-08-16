e602745d065cce2209a624fb001f04e471cdffbe
Jeff Triplett <jeff.triplett@gmail.com>
2020-08-15 23:53:25 -0500

    :elephant: Updates migrations for Django 1.8

---
+++ members/migrations/0005_auto_20200815_2351.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('members', '0004_auto_20200724_1500'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='member',
+            name='email',
+            field=models.EmailField(max_length=254, null=True, blank=True),
+        ),
+    ]
