0c9963fe74f8ce79859e7df04a3c018b6eeeaf8e
Jeff Triplett <jeff.triplett@gmail.com>
2020-08-15 22:37:36 -0500

    :tractor: Adds back some settings

---
+++ config/settings_hawkhundred.py
@@ -0,0 +1,14 @@
+import os
+
+from config.settings import *
+
+
+SITE_ID = 2
+
+ROOT_URLCONF = 'config.urls_hawkhundred'
+
+TEMPLATE_DIRS = [
+    os.path.join(TEMPLATE_ROOT, 'hawkhundred.com'),
+    os.path.join(TEMPLATE_ROOT, 'race_defaults'),
+    os.path.join(TEMPLATE_ROOT, 'defaults'),
+]
