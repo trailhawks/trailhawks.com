69b9a77d34ea2ba085a0dcb8fdb12c5b4a6dcbcb
Jeff Triplett <jeff.triplett@gmail.com>
2021-06-08 23:41:35 -0500

    :tractor: Migrates docopt management cmds to django-click

--- races/management/commands/load_race_results.py
+++ races/management/commands/load_race_results.py
@@ -1,97 +1,82 @@
 import csv
+import djclick as click
 import logging
 
-from django_docopt_command import DocOptCommand
-
 from races.models import Race, RaceType, Racer, Result
-
-
-__doc__ = """Usage:
-load_race_results --race=<race> --csv=<csv> [--update]
-
-Options:
-  -h --help       Show this screen.
-  --version       Show version.
-  --csv=<csv>     CSV file to load results from.
-  --race=<race>   Race ID or Race slug to associate the results with.
-"""
 
 
 logger = logging.getLogger(__name__)
 
 
-class Command(DocOptCommand):
+@click.command()
+@click.option('--race')
+@click.option('csv_filename', '--csv')
+@click.option('--update', default=False)
+def command(race, csv_filename, update):
+    logger.info(race)
 
-    docs = __doc__
+    if not csv_filename:
+        raise Exception("You forgot the CSV silly")
 
-    def handle_docopt(self, arguments):
-        race = arguments["--race"]
-        csv_filename = arguments["--csv"]
+    if not race:
+        raise Exception("You forgot to add the Race silly")
 
-        logger.info(race)
+    try:
+        race = Race.objects.get(pk=int(race))
+    except ValueError:
+        race = Race.objects.get(slug=race)
 
-        if not csv_filename:
-            raise Exception("You forgot the CSV silly")
+    with open(csv_filename, "r") as f:
+        result_data = list(csv.DictReader(f))
 
-        if not race:
-            raise Exception("You forgot to add the Race silly")
+    for row in result_data:
+        defaults = {}
+        race_type = None
+        bib_number = row["bib"]
+        time = row["time"]
+        place = row["comments"]
+        first_name = row["first_name"]
+        last_name = row["last_name"]
+        distance = row["distance"]
 
-        try:
-            race = Race.objects.get(pk=int(race))
-        except ValueError:
-            race = Race.objects.get(slug=race)
+        if row["gender"].strip() in ["m", "M"]:
+            gender = 1
+        else:
+            gender = 2
 
-        with open(csv_filename, "r") as f:
-            result_data = list(csv.DictReader(f))
+        racer, _ = Racer.objects.get_or_create(
+            first_name=first_name, last_name=last_name, gender=gender
+        )
 
-        for row in result_data:
-            defaults = {}
-            race_type = None
-            bib_number = row["bib"]
-            time = row["time"]
-            place = row["comments"]
-            first_name = row["first_name"]
-            last_name = row["last_name"]
-            distance = row["distance"]
+        if len(distance):
+            race_type, _ = RaceType.objects.get_or_create(name=distance)
 
-            if row["gender"].strip() in ["m", "M"]:
-                gender = 1
-            else:
-                gender = 2
+        if "CR" in place:
+            defaults["course_record"] = True
 
-            racer, _ = Racer.objects.get_or_create(
-                first_name=first_name, last_name=last_name, gender=gender
-            )
+        if "DNF" in time:
+            defaults["dnf"] = True
 
-            if len(distance):
-                race_type, _ = RaceType.objects.get_or_create(name=distance)
+        if "DNS" in time:
+            defaults["dns"] = True
 
-            if "CR" in place:
-                defaults["course_record"] = True
+        logger.info("Found Racer: {0}".format(racer))
 
-            if "DNF" in time:
-                defaults["dnf"] = True
+        # might update_or_create...
+        result, _ = Result.objects.get_or_create(
+            race=race,
+            racer=racer,
+            time=time,
+            bib_number=bib_number,
+            place=place,
+            defaults=defaults,
+        )
 
-            if "DNS" in time:
-                defaults["dns"] = True
+        if race_type:
+            result.race_type = race_type
+            result.save()
 
-            logger.info("Found Racer: {0}".format(racer))
+        logger.info("Result for {0} for race: {1}".format(racer, race))
+        logger.info(result)
 
-            # might update_or_create...
-            result, _ = Result.objects.get_or_create(
-                race=race,
-                racer=racer,
-                time=time,
-                bib_number=bib_number,
-                place=place,
-                defaults=defaults,
-            )
-
-            if race_type:
-                result.race_type = race_type
-                result.save()
-
-            logger.info("Result for {0} for race: {1}".format(racer, race))
-            logger.info(result)
-
-        logger.info("results loaded")
+    logger.info("results loaded")
