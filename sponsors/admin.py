fb277a3d21aa9bb6c125d3a35443c40eb261afd6
Jeff Triplett <jeff.triplett@gmail.com>
2021-06-12 22:32:40 -0500

    :pencil: Adds some new sponsors actions to help manage them easier

--- sponsors/admin.py
+++ sponsors/admin.py
@@ -4,12 +4,34 @@ from django.contrib.contenttypes.admin import GenericStackedInline
 from .models import Sponsor
 
 
+def set_homepage_to_true(modeladmin, request, queryset):
+    queryset.update(homepage=True)
+
+
+def set_homepage_to_false(modeladmin, request, queryset):
+    queryset.update(homepage=False)
+
+
+def set_active_to_true(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+
+def set_active_to_false(modeladmin, request, queryset):
+    queryset.update(active=False)
+
+
 class SponsorInline(GenericStackedInline):
     model = Sponsor
     extra = 0
 
 
 class SponsorAdmin(admin.ModelAdmin):
+    actions = [
+        set_active_to_false,
+        set_active_to_true,
+        set_homepage_to_false,
+        set_homepage_to_true,
+    ]
     prepopulated_fields = {"slug": ["name"]}
     list_display = ("name", "active", "homepage", "content_type", "object_id")
     list_filter = (
