cf78fff528760bcc687b8d51726791273b7ed367
Jeff Triplett <jeff.triplett@gmail.com>
2021-06-12 22:32:51 -0500

    :shirt: Lints sponsors

--- sponsors/models.py
+++ sponsors/models.py
@@ -3,9 +3,9 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 from .managers import SponsorManager
-from django.utils.translation import gettext_lazy as _
 
 
 class Sponsor(models.Model):
