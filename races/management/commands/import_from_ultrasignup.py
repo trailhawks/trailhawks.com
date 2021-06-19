b64b1b1396b5d518057c8a30fac9dd326fcfdb88
Jeff Triplett <jeff.triplett@gmail.com>
2021-06-19 14:27:52 -0500

    :warning: This is a wip update which is probably not quite baked

--- races/management/commands/import_from_ultrasignup.py
+++ races/management/commands/import_from_ultrasignup.py
@@ -8,67 +8,72 @@ from races.models import Race, Racer, RaceType, Result
 
 
 @click.command()
-@click.argument("result_id")
+# @click.argument("result_id")
+# @click.option("race_slug", "--slug", "--race-slug")
+# @click.option("race_type_name", "--race-type")
 @click.option("race_id", "--race", "--race-id", "--race-pk")
-@click.option("race_slug", "--slug", "--race-slug")
-@click.option("race_type_name", "--race-type")
-def command(result_id, race_id, race_slug, race_type_name):
+def command(race_id):
     headers = {"Content-type": "application/json"}
-    url = f"https://ultrasignup.com/service/events.svc/results/{result_id}/1/json"
 
-    if not Path("results").exists():
-        Path("results").mkdir()
+    races = Race.objects.all().exclude(ultrasignup_id=None)
+    for race in races:
+        print(f"{race.pk}=={race.ultrasignup_id}")
+        url = f"https://ultrasignup.com/service/events.svc/results/{race.ultrasignup_id}/1/json"
 
-    filename = Path("results", f"{result_id}.json")
+        if not Path("_results").exists():
+            Path("_results").mkdir()
 
-    if filename.exists():
-        input_buffer = json.loads(filename.read_text())
-    else:
-        request = requests.get(url, headers=headers)
-        input_buffer = request.json()
-        filename.write_text(json.dumps(input_buffer, indent=2))
+        filename = Path("_results", f"{race.ultrasignup_id}.json")
 
-    try:
-        if race_id:
-            race = Race.objects.get(pk=race_id)
-        elif race_slug:
-            race = Race.objects.get(slug=race_slug)
+        if filename.exists():
+            input_buffer = json.loads(filename.read_text())
         else:
-            raise Exception("We need a Race <pk> or <slug>")
-    except Race.DoesNotExist:
-        raise Exception(f"Race <pk:{race_id}> or <slug:{race_slug}> not found")
+            request = requests.get(url, headers=headers)
+            input_buffer = request.json()
+            filename.write_text(json.dumps(input_buffer, indent=2))
 
-    race_type, _ = RaceType.objects.get_or_create(name=race_type_name)
+        if len(input_buffer) > 0:
 
-    for item in input_buffer:
-        # TODO: try to track by member too...
-        racer, _ = Racer.objects.update_or_create(
-            first_name=item["firstname"],
-            last_name=item["lastname"],
-            defaults={
-                "gender": Racer.MALE if item["gender"] == "M" else Racer.FEMALE,
-                "city": item["city"],
-                "state": item["state"],
-                "country": "USA",
-            },
-        )
+            # try:
+            #     if race_id:
+            #         race = Race.objects.get(pk=race_id)
+            #     elif race_slug:
+            #         race = Race.objects.get(slug=race_slug)
+            #     else:
+            #         raise Exception("We need a Race <pk> or <slug>")
+            # except Race.DoesNotExist:
+            #     raise Exception(f"Race <pk:{race_id}> or <slug:{race_slug}> not found")
 
-        formattime = item["formattime"]
+            # race_type, _ = RaceType.objects.get_or_create(name=race_type_name)
 
-        if len(formattime) in [1, 2]:
-            formattime = ""
+            for item in input_buffer:
+                racer, _ = Racer.objects.update_or_create(
+                    first_name=item["firstname"],
+                    last_name=item["lastname"],
+                    defaults={
+                        "city": item["city"],
+                        "country": "USA",
+                        "gender": Racer.MALE if item["gender"] == "M" else Racer.FEMALE,
+                        "state": item["state"],
+                    },
+                )
 
-        elif len(formattime) == 7:
-            formattime = f"0{formattime}"
+                formattime = item["formattime"]
 
-        result, _ = Result.objects.update_or_create(
-            race=race,
-            race_type=race_type,
-            racer=racer,
-            defaults={
-                "bib_number": item["bib"] or None,
-                "time": formattime,
-                "dnf": item["status"] == 3,
-                "dns": item["status"] == 2,
-            },
-        )
+                if len(formattime) in [1, 2]:
+                    formattime = ""
+
+                elif len(formattime) == 7:
+                    formattime = f"0{formattime}"
+
+                result, _ = Result.objects.update_or_create(
+                    race=race,
+                    # race_type=race_type,
+                    racer=racer,
+                    defaults={
+                        "bib_number": item["bib"] or None,
+                        "dnf": item["status"] == 3,
+                        "dns": item["status"] == 2,
+                        "time": formattime,
+                    },
+                )
