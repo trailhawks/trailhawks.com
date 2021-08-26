82c2b620b9f849413fe490d2b7fce9f13c2aa853
Jeff Triplett <jeff.triplett@gmail.com>
2021-08-26 18:56:45 -0500

    :pencil: Updates static paths

--- config/urls.py
+++ config/urls.py
@@ -121,5 +121,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
