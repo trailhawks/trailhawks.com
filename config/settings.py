7efa95b896499f2546ed9ee671e3f8fa91095475
Jeff Triplett <jeff.triplett@gmail.com>
2021-08-26 19:52:47 -0500

    :fire: Remove logging to get 500s to bubble up

--- config/settings.py
+++ config/settings.py
@@ -128,42 +128,42 @@ INSTALLED_APPS += [
 
 # Logging
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "formatters": {
-        "verbose": {
-            "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
-        },
-        "simple": {"format": "%(levelname)s %(message)s"},
-    },
-    "handlers": {
-        "null": {"level": "DEBUG", "class": "logging.NullHandler"},
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
-        "mail_admins": {
-            "level": "ERROR",
-            "class": "django.utils.log.AdminEmailHandler",
-            # 'filters': ['special']
-        },
-    },
-    "loggers": {
-        "django": {"handlers": ["null"], "propagate": True, "level": "INFO"},
-        "django.request": {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
-            "propagate": False,
-        },
-        "trailhawks": {
-            "handlers": ["console"],
-            "level": "DEBUG",
-            # 'filters': ['special']
-        },
-    },
-}
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": True,
+#     "formatters": {
+#         "verbose": {
+#             "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
+#         },
+#         "simple": {"format": "%(levelname)s %(message)s"},
+#     },
+#     "handlers": {
+#         "null": {"level": "DEBUG", "class": "logging.NullHandler"},
+#         "console": {
+#             "level": "DEBUG",
+#             "class": "logging.StreamHandler",
+#             "formatter": "verbose",
+#         },
+#         "mail_admins": {
+#             "level": "ERROR",
+#             "class": "django.utils.log.AdminEmailHandler",
+#             # 'filters': ['special']
+#         },
+#     },
+#     "loggers": {
+#         "django": {"handlers": ["null"], "propagate": True, "level": "INFO"},
+#         "django.request": {
+#             "handlers": ["mail_admins"],
+#             "level": "ERROR",
+#             "propagate": False,
+#         },
+#         "trailhawks": {
+#             "handlers": ["console"],
+#             "level": "DEBUG",
+#             # 'filters': ['special']
+#         },
+#     },
+# }
 
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
