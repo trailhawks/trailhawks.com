39284619dd78c2da814a4c97c2af83cd8964e489
Jeff Triplett <jeff.triplett@gmail.com>
2025-12-31 22:30:06 -0600

    Add blessed dependency and update healthcheck-worker.sh with --skip-checks

--- healthcheck-worker.sh
+++ healthcheck-worker.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Check if the Q cluster is alive
-uv run -m manage qinfo | grep -q "Workers" && exit 0
+uv run -m manage qinfo --skip-checks | grep -q "Workers" && exit 0
 
 echo "Django-Q2 cluster not running"
 exit 1
