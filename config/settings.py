93d0436bbf5fe333c06cecee98fa0d5fd2d904b6
Jeff Triplett <jeff.triplett@gmail.com>
2026-01-05 12:05:06 -0600

    Standardize STATICFILES_DIRS to use frontend/

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ MEDIA_URL = "/media/"
 
 STATIC_ROOT = str(BASE_DIR.joinpath("static_root"))
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("assets"))]
+STATICFILES_DIRS = (str(BASE_DIR.joinpath("frontend")),)
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
